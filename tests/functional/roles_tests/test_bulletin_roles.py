ae052fed97977bef9fb5e68087220629d17ad9c8
Owen Nugent <owen@projectcastrumoutlook.onmicrosoft.com>
2024-04-29 08:41:19 -0400

    adds tests on update bulletin

--- tests/functional/roles_tests/test_bulletin_roles.py
+++ tests/functional/roles_tests/test_bulletin_roles.py
@@ -3,7 +3,6 @@ import json
 import uuid
 from conftest import (ADMIN_ROLE_NAME, DA_ROLE_NAME, MOD_ROLE_NAME, CUSTOM_ROLE_NAME, SECOND_CUSTOM_ROLE_NAME, 
                       create_user_with_roles, create_user_session)
-from bs4 import BeautifulSoup
 
 def create_bulletin_requiring_roles(auth_session, roles_by_role_name, required_role_names):
   bulletin = bulletin = {
@@ -58,14 +57,64 @@ def create_bulletin_requiring_roles(auth_session, roles_by_role_name, required_r
   ([CUSTOM_ROLE_NAME], [CUSTOM_ROLE_NAME], 200),
 ])
 def test_getBulletin_withAssignedUserRolesAndRequiredBulletinAccessRestrictions(test_flask_app, auth_session, roles_by_role_name, required_roles, assigned_roles, expected_api_response_code):
-  bulletin = create_bulletin_requiring_roles(auth_session, roles_by_role_name, required_roles)
+  bulletin_id = create_bulletin_requiring_roles(auth_session, roles_by_role_name, required_roles)
   user_roles = []
   for role_name in assigned_roles:
     user_roles.append(roles_by_role_name[role_name])
   username = create_user_with_roles(auth_session, user_roles)
   user_session = create_user_session(test_flask_app, username)
+  response = user_session.get(F'/admin/api/bulletin/{bulletin_id}',
+                               headers={"Content-Type": "application/json"})
+  assert response.status_code == expected_api_response_code
 
-  response = user_session.get(F'/admin/api/bulletin/{bulletin}',
+@pytest.mark.parametrize("assigned_roles, expected_api_response_code", [
+  ([ADMIN_ROLE_NAME], 200),
+  ([MOD_ROLE_NAME], 403),
+  ([DA_ROLE_NAME], 403),
+  ([CUSTOM_ROLE_NAME], 403),
+  ([], 403)
+])
+def test_deleteBulletin_withAssignedUserRoles(test_flask_app, auth_session, roles_by_role_name, assigned_roles, expected_api_response_code):
+  bulletin_id = create_bulletin_requiring_roles(auth_session, {}, [])
+  user_roles = []
+  for role_name in assigned_roles:
+    user_roles.append(roles_by_role_name[role_name])
+  username = create_user_with_roles(auth_session, user_roles)
+  user_session = create_user_session(test_flask_app, username)
+  response = user_session.delete(F'/admin/api/bulletin/{bulletin_id}')
+  assert response.status_code == expected_api_response_code
+
+@pytest.mark.parametrize("required_roles, assigned_roles, expected_api_response_code", [
+  # BELOW IS A LIST OF TEST CASE INPUTS. ABOVE IS THE FORMAT OF TEST CASE INPUTS
+  ([ADMIN_ROLE_NAME], [], 403),
+  ([ADMIN_ROLE_NAME], [MOD_ROLE_NAME], 403),
+  ([ADMIN_ROLE_NAME], [MOD_ROLE_NAME, CUSTOM_ROLE_NAME, DA_ROLE_NAME], 403),
+  ([ADMIN_ROLE_NAME], [ADMIN_ROLE_NAME], 200),
+  ([ADMIN_ROLE_NAME], [ADMIN_ROLE_NAME, MOD_ROLE_NAME], 200),
+  ([], [], 403), # read only users do not have write permissions
+  ([], [CUSTOM_ROLE_NAME], 403), # read only users do not have write permissions
+  ([CUSTOM_ROLE_NAME], [], 403),
+  ([CUSTOM_ROLE_NAME], [SECOND_CUSTOM_ROLE_NAME], 403),
+  ([CUSTOM_ROLE_NAME], [MOD_ROLE_NAME], 403),
+  ([CUSTOM_ROLE_NAME], [DA_ROLE_NAME], 403),
+  ([CUSTOM_ROLE_NAME], [ADMIN_ROLE_NAME], 200),
+  ([CUSTOM_ROLE_NAME], [CUSTOM_ROLE_NAME], 403), # read only users do not have write permisisons
+])
+def test_updateBulletin_withAssignedUserRolesAndRequiredBulletinAccessRestrictions(test_flask_app, auth_session, roles_by_role_name, required_roles, assigned_roles, expected_api_response_code):
+  bulletin_id = create_bulletin_requiring_roles(auth_session, roles_by_role_name, required_roles)
+  user_roles = []
+  for role_name in assigned_roles:
+    user_roles.append(roles_by_role_name[role_name])
+  username = create_user_with_roles(auth_session, user_roles)
+  user_session = create_user_session(test_flask_app, username)
+  bulletin = bulletin = {
+    "title": str(uuid.uuid4()),
+    "status": "Human Created",
+    "description": str(uuid.uuid4())
+  }
+  payload = json.dumps({"item": bulletin})
+  response = user_session.put(F'/admin/api/bulletin/{bulletin_id}',
+                               data = payload,
                                headers={"Content-Type": "application/json"})
   assert response.status_code == expected_api_response_code
 
@@ -105,7 +154,6 @@ def test_bulletinQuery_bulletinsAreRestrictedByRoles(test_flask_app, auth_sessio
                                data = json.dumps(query_payload),
                                headers={"Content-Type": "application/json"})
   assert response.status_code == 200
-  print(response.data)
   bulletins = json.loads(response.data)['items']
   bulletin_1_found = False
   bulletin_2_found = False
@@ -126,16 +174,8 @@ def test_bulletinQuery_bulletinsAreRestrictedByRoles(test_flask_app, auth_sessio
   assert bulletin_3_found
   assert bulletin_4_found == False
 
-
-# consider views
-# consider import/export
-# consider UI components?
-
 # check role CRUD operations
-
 # test role assignment
 
 # some endpoints e.g. @admin.post('/api/location/') are decorated with @roles_accepted('Admin', 'Mod', 'DA') that on its face appears to only consider built-in roles
 # are these endpoints compatible with custom roles?
-
-# looking via endpoint availability, which data types can have limited access via CAESAR roles?
