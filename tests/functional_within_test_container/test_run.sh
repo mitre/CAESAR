4afcb985eea7134290f013a3c3bfa34d0b841c19
Owen Nugent <owen@projectcastrumoutlook.onmicrosoft.com>
2024-05-21 18:37:10 -0400

    I have issues running these tests as they were on windows because of permissions issues on the postgres container(?). Removing the variable input options to create a stable named volume fixes this for me. I will check that this is ok for .devcontainer and anything else

--- tests/functional_within_test_container/test_run.sh
+++ tests/functional_within_test_container/test_run.sh
@@ -1,7 +1,4 @@
-test_run_project_name="test_run_"`date +"%m_%d_%Y_%H_%M"`
-
-# Export DB_DATA so the `docker compose` commands pick it up
-export DB_DATA="./pgdata_${test_run_project_name}"
+test_run_project_name="test_run_"`date +"%m_%d_%Y_%H_%M_%S"`
 
 # Start up PostgreSQL and Redis and give them 10 seconds to start running
 BAYANAT_EXTERNAL_PORT=5001 POSTGRES_EXTERNAL_PORT=5433 REDIS_EXTERNAL_PORT=6380 docker compose -p $test_run_project_name up -d postgres redis
@@ -17,6 +14,5 @@ docker compose -p $test_run_project_name exec --user root bayanat pip install -r
 # Run the tests
 docker compose -p $test_run_project_name exec --user root bayanat python -m pytest tests/functional_within_test_container
 
-# Stop and remove the containers and remove the DB_DATA folder
+# Stop and remove the containers
 docker compose -p $test_run_project_name down -v
-rm -rf $DB_DATA
