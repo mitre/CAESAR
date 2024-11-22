ab598c74382af7652eb11e2f285dc89c13f3b006
Stephen Purdy <sfpurdy@mitre.org>
2024-11-22 13:08:19 -0500

    Change the options for the Actor Civilian / Non-civilian field

---
+++ migrations/versions/791ab40d36f7_change_actor_civilian_options.py
@@ -0,0 +1,35 @@
+"""Change actor.civilian options
+
+Revision ID: 791ab40d36f7
+Revises: 20203b99987f
+Create Date: 2024-11-14 19:47:49.233714
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '791ab40d36f7'
+down_revision = '20203b99987f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # Unset actor.civilian when it's 'Non-Civilian'
+    op.execute(f"UPDATE actor SET civilian=NULL WHERE civilian='Non-Civilian';")
+
+
+def downgrade():
+    # Unset actor.civilian when it's 'Legislator'
+    op.execute(f"UPDATE actor SET civilian=NULL WHERE civilian='Legislator';")
+    
+    # Unset actor.civilian when it's 'Govt Official (non-military)'
+    op.execute(f"UPDATE actor SET civilian=NULL WHERE civilian='Govt Official (non-military)';")
+    
+    # Unset actor.civilian when it's 'Military'
+    op.execute(f"UPDATE actor SET civilian=NULL WHERE civilian='Military';")
+    
+    # Unset actor.civilian when it's 'Paramilitary'
+    op.execute(f"UPDATE actor SET civilian=NULL WHERE civilian='Paramilitary';")
